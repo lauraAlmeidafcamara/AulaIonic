14e3934821b0b5102f9c8f152bdd12591834313d
Laura Damaceno de Almeida <laura.almeida@fcamara.com.br>
2018-05-02 09:48:30 -0300

    alterações do post

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,46 @@
 import { Component } from '@angular/core';
-import {Http, Response} from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { NavController } from 'ionic-angular';
-import{HttpClient, HttpHeaders} from '@angular/common/http'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TodosService } from '../service/service';
 @Component({
- 
+
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  todo:any;
-  constructor(public navCtrl: NavController, public TodosService: TodosService, public httpClient:HttpClient ) {}  
+  todo: any;
+  frase: string = "";
+  userId: number;
+  id: number;
+  constructor(public navCtrl: NavController, public TodosService: TodosService) { }
 
-  ionViewDidLoad(){
-   this.httpClient.get("http://localhost:3000/todos")
-    .subscribe(
-      (res) => { this.todo = res }, 
-      (err) => { console.log(err) }
-   );
- }
+
+  async ionViewDidLoad() {
+    await this.getAll();
+  }
+
+  async getAll(){
+    await this.TodosService.get().subscribe((res) => {
+      console.log(res);
+      this.todo = res;
+    });
+  }
+
+  async post() {
+    let todo = {
+      completed: true,
+      id: this.id,
+      userId: this.userId,
+      title: this.frase
+    }
+
+    await this.TodosService.post(todo).subscribe((res) => {
+      console.log(res);
+    });
+
+
+    await this.getAll();
+  }
 
 }
-        
--- src/pages/service/service.ts
+++ src/pages/service/service.ts
@@ -2,14 +2,28 @@ import { Http } from "@angular/http";
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
+import { PromiseObservable } from "rxjs/observable/PromiseObservable";
+import { Title } from "@angular/platform-browser";
 
 @Injectable()
 export class TodosService {
     private url: string = 'http://localhost:3000/todos';
     private headers = new HttpHeaders({ 'Content-type': 'application/json' });
     private options = { headers: this.headers };
-    constructor(public http: HttpClient) {}
-    service() {
-        return this.http.get(this.url, this.options)
+    
+    constructor(public http: HttpClient) {
+        var currentUser = JSON.parse(localStorage.getItem('frase'));
+       
     }
-}  
+
+    get() {
+        return this.http.get(this.url, this.options);
+    }
+
+    post(todo){
+        return this.http.post(this.url, todo, this.options);
+    }
+
+    }
